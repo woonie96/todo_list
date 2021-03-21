4b0832a7295c52232b0cf0ebf5923f7cf7cdb89a
woonie96 <woonstar@seoultech.ac.kr>
2021-03-21 11:17:04 +0900

    Initial Commit

---
+++ todo_list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo_list
+//
+//  Created by JeongWoonSeong on 2021/03/21.
+//  Copyright © 2021 JeongWoonSeong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
